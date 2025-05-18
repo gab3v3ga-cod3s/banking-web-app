9c837886c18b99687fbc6bdf0680613395b33a36
gabe-vega <146376071+gab3v3ga-cod3s@users.noreply.github.com>
2025-05-18 19:21:41 -0400

    implement home/dash screen items like welcome message, including headerbox usage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+    const loggedIn = { firstName: 'Gabe' };
+    return (
+        <section className="home">
+            <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Efficiently access and manage your account and transactions"
+                />
+            </header>
+            </div>
+        </section>
+    )
 }
 
 export default Home
