4f43e6d7348251505f4444d0a4dd1cd0b4e5705e
gabe-vega <146376071+gab3v3ga-cod3s@users.noreply.github.com>
2025-05-21 23:21:04 -0400

    implement mobile navigation sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,6 +21,7 @@ const Home = () => {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+            RECENT TRANSACTIONS
             </div>
         </section>
     )
