abd6bc03d769213c87e6ad300bb3811a5271be03
gabe-vega <146376071+gab3v3ga-cod3s@users.noreply.github.com>
2025-05-21 21:54:27 -0400

    implement animated counter using CountUp react component

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import AnimatedCounter from './AnimatedCounter';
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -14,6 +14,15 @@ const TotalBalanceBox = ({
         <h2 className="header-2">
             Bank Accounts: {totalBanks} 
         </h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">
+            Total Current Balance
+          </p>
+          <p className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount=
+            {totalCurrentBalance}/>
+          </p>
+        </div>
       </div>
     </section>
   )
