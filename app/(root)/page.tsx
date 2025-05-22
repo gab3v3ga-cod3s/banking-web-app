0df12ffcadba340b2585f95f1a6811fb48e70389
gabe-vega <146376071+gab3v3ga-cod3s@users.noreply.github.com>
2025-05-21 23:29:58 -0400

    begin implementation of right sidebar; sidebar created with name visible

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { Lasso } from 'lucide-react';
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Gabe' };
+    const loggedIn = { firstName: 'Gabe', lastName: 'Vega', email: 'gabev1303@gmail.com' };
     return (
         <section className="home">
             <div className="home-content">
@@ -23,6 +25,11 @@ const Home = () => {
             </header>
             RECENT TRANSACTIONS
             </div>
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[]}
+            />
         </section>
     )
 }
