4da96b816e82a93c85d6a9861758c77f9d453cba
gabe-vega <146376071+gab3v3ga-cod3s@users.noreply.github.com>
2025-05-21 22:09:31 -0400

    implement doughnut chart with dummy values (for now)

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,6 @@
+import { Doughnut } from 'react-chartjs-2';
 import AnimatedCounter from './AnimatedCounter';
+import DoughnutChart from './DoughnutChart';
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -6,8 +8,7 @@ const TotalBalanceBox = ({
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
-        {/* Doughnut Chart*/}
-
+        <DoughnutChart accounts={accounts}/>
       </div>
 
       <div className="flex flex-col gap-6">
@@ -18,10 +19,10 @@ const TotalBalanceBox = ({
           <p className="total-balance-label">
             Total Current Balance
           </p>
-          <p className="total-balance-amount flex-center gap-2">
+          <div className="total-balance-amount flex-center gap-2">
             <AnimatedCounter amount=
             {totalCurrentBalance}/>
-          </p>
+          </div>
         </div>
       </div>
     </section>
